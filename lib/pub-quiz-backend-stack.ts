555ad76612d7d4dce58c3ac093d48b9bd2b878f5
Edward Byne <edward.byne@codecentric.de>
2020-06-16 23:23:03 +0200

    Initial setup of appsync api

--- lib/pub-quiz-backend-stack.ts
+++ lib/pub-quiz-backend-stack.ts
@@ -1,9 +1,19 @@
 import * as cdk from '@aws-cdk/core';
+import { FieldLogLevel, GraphQLApi } from '@aws-cdk/aws-appsync';
+import * as Path from 'path';
 
 export class PubQuizBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new GraphQLApi(this, 'Api', {
+      name: 'pub-quiz-api',
+      logConfig: {
+        fieldLogLevel: FieldLogLevel.ALL,
+      },
+      schemaDefinitionFile: Path.join(__dirname, './schema.graphql'),
+    });
+
+    api.addNoneDataSource('None', 'Dummy data source');
   }
 }
--- test/pub-quiz-backend.test.ts
+++ test/pub-quiz-backend.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as PubQuizBackend from '../lib/pub-quiz-backend-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new PubQuizBackend.PubQuizBackendStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new PubQuizBackend.PubQuizBackendStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
