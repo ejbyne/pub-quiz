a0f6b3bb0c564b3c39c2351e132809ae4acf122a
Edward Byne <edward.byne@codecentric.de>
2020-06-26 17:23:08 +0200

    Implement next state resolver

--- src/lambdas/nextQuizState.ts
+++ src/lambdas/nextQuizState.ts
@@ -1,5 +1,6 @@
 import { Handler } from 'aws-lambda';
 import { QuizRepository } from '../repositories/QuizRepository';
+import { QuizState, QuizStatus } from '../domain/types';
 
 interface Event {
   arguments: {
@@ -9,10 +10,33 @@ interface Event {
   };
 }
 
+interface NextStateEvent {
+  quizId: string;
+  status: QuizStatus;
+  roundNumber?: number;
+  roundName?: string;
+  numberOfQuestions?: number;
+  questionNumber?: number;
+  questionText?: string;
+}
+
 const quizTableName = process.env.QUIZ_TABLE_NAME as string;
 
 const quizRepository = new QuizRepository(quizTableName);
 
-export const nextState: Handler<Event> = async (event: Event): Promise<any> => {
+export const nextState: Handler<Event> = async (
+  event: Event
+): Promise<NextStateEvent> => {
   const { quizId } = event.arguments.input;
+
+  const quiz = await quizRepository.get(quizId);
+
+  const nextState = quiz.nextState;
+
+  await quizRepository.updateState(quizId, nextState);
+
+  return {
+    quizId,
+    ...nextState,
+  };
 };
