c4047689f87efc10b73202412bd8fa287090a423
Edward Byne <edward.byne@codecentric.de>
2020-12-24 23:39:21 +0100

    Add empty admin page

--- web-app/src/components/AppContainer.tsx
+++ web-app/src/components/AppContainer.tsx
@@ -1,21 +1,32 @@
-import React, { useReducer, Reducer } from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { App } from './App';
-import { Quiz, QuizAction } from '../shared/domain/types';
-import { quizReducer } from '../shared/domain/quizReducer';
-import { QuizContext } from '../shared/context/quizContext';
-import { client } from '../shared/graphql/apolloClient';
+import React, { useReducer, Reducer } from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { App } from "./App";
+import { Quiz, QuizAction } from "../shared/domain/types";
+import { quizReducer } from "../shared/domain/quizReducer";
+import { QuizContext } from "../shared/context/quizContext";
+import { client } from "../shared/graphql/apolloClient";
+import { Admin } from "./Admin";
 
 export const AppContainer: React.FC = () => {
   const [quiz, updateQuiz] = useReducer<Reducer<Quiz, QuizAction>>(
     quizReducer,
-    { rounds: [] },
+    { rounds: [] }
   );
 
   return (
     <ApolloProvider client={client}>
       <QuizContext.Provider value={[quiz, updateQuiz]}>
-        <App />
+        <Router>
+          <Switch>
+            <Route path="/admin">
+              <Admin />
+            </Route>
+            <Route path="/">
+              <App />
+            </Route>
+          </Switch>
+        </Router>
       </QuizContext.Provider>
     </ApolloProvider>
   );
