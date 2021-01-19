089e89ff8acb4742b31f3165b39616381a884fb9
Edward Byne <ejbyne@gmail.com>
2021-01-19 15:14:53 +0100

    Enable react refresh

--- web-app/razzle.config.js
+++ web-app/razzle.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 
 module.exports = {
+  experimental: {
+    reactRefresh: true,
+  },
+
   modifyWebpackConfig({ webpackConfig }) {
     return {
       ...webpackConfig,
