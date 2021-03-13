59798d0ee9086bee57910f554fd36fc801405e7b
Edward Byne <edward.byne@codecentric.de>
2021-03-13 23:18:34 +0100

    Show marks in mark sheet

--- web-app/src/components/MarkSheet.tsx
+++ web-app/src/components/MarkSheet.tsx
@@ -11,13 +11,41 @@ export const MarkSheet: React.FC<{}> = () => {
   const state = quiz.state as RoundMarked | QuizFinished;
 
   return (
-    <section className="w-full lg:w-1/2 px-6 py-6 flex flex-col items-stretch bg-indigo-900 lg:shadow-2xl lg:rounded-lg flex-grow text-gray-200 overflow-y-auto">
+    <section className="w-full lg:w-1/2 px-6 py-6 flex flex-col bg-indigo-900 lg:shadow-2xl lg:rounded-lg flex-grow text-gray-200">
       <h1 className="text-2xl text-center mb-4">
         {state.status === QuizStatus.RoundMarked
           ? `Round ${(state as RoundMarked).roundSummary.roundNumber + 1}`
           : 'Quiz complete'}
       </h1>
-      <h2 className="text-xl text-center mb-4">Marks</h2>
+      {state.marks && (
+        <>
+          <h2 className="text-xl text-center mb-4">Marks</h2>
+          <div className="w-full overflow-x-auto">
+            <table className="text-left table-fixed">
+              <thead>
+                <tr>
+                  <th className="w-24 px-1 align-top">Player</th>
+                  <th className="w-24 px-1 align-top">Total</th>
+                  {state.marks[0]?.rounds.map((_, index) => (
+                    <th className="w-24 px-1 align-top">Round {index + 1}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {state.marks.map((playerMarks) => (
+                  <tr>
+                    <td className="px-1 align-top">{playerMarks.playerName}</td>
+                    <td className="px-1 align-top">{playerMarks.quizTotal}</td>
+                    {playerMarks.rounds.map((round) => (
+                      <td className="px-1 align-top">{round.roundTotal}</td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
     </section>
   );
 };
