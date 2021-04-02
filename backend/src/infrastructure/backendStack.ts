01850797e3d8062c83f9c1f7fa712bc6365d2995
Edward Byne <edward.byne@codecentric.de>
2021-04-02 23:37:12 +0200

    Add user pool for quiz hosts

--- backend/src/infrastructure/backendStack.ts
+++ backend/src/infrastructure/backendStack.ts
@@ -3,10 +3,17 @@ import { FieldLogLevel, GraphqlApi, Schema } from '@aws-cdk/aws-appsync';
 import { BillingMode, Table, AttributeType } from '@aws-cdk/aws-dynamodb';
 import { createLambdaResolvers } from './createLambdaResolvers';
 import { CfnOutput } from '@aws-cdk/core';
+import {
+  UserPool,
+  UserPoolClient,
+  VerificationEmailStyle,
+} from '@aws-cdk/aws-cognito';
 
 export class PubQuizBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // API
 
     const api = new GraphqlApi(this, 'Api', {
       name: 'pub-quiz-api',
@@ -28,8 +35,40 @@ export class PubQuizBackendStack extends cdk.Stack {
 
     createLambdaResolvers(this, quizTable, api);
 
-    new CfnOutput(this, 'graphQlUrl', {
+    // Cognito
+
+    const userPool = new UserPool(this, 'UserPool', {
+      userPoolName: 'pub-quiz-users',
+      selfSignUpEnabled: true,
+      userVerification: {
+        emailSubject: 'The Online Pub Quiz - email verification',
+        emailBody:
+          'Thanks for signing up to the Online Pub Quiz! Your verification code is {####}.',
+        emailStyle: VerificationEmailStyle.CODE,
+      },
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+    });
+
+    const userPoolClient = new UserPoolClient(this, 'UserPoolClient', {
+      userPool,
+      authFlows: {
+        userSrp: true,
+      },
+    });
+
+    // Outputs
+
+    new CfnOutput(this, 'GraphQlUrl', {
       value: api.graphqlUrl,
+    });
+
+    new CfnOutput(this, 'UserPoolId', {
+      value: userPool.userPoolId,
+    });
+
+    new CfnOutput(this, 'UserPoolClientId', {
+      value: userPoolClient.userPoolClientId,
     });
   }
 }
