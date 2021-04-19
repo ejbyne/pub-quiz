c143a9f3cc085cf75b454466f4a2e2c274d64964
Edward Byne <edward.byne@codecentric.de>
2021-04-19 23:08:59 +0200

    Comment out random quiz generator

--- web-app/src/components/Admin.tsx
+++ web-app/src/components/Admin.tsx
@@ -8,22 +8,22 @@ import { Layout } from './Layout';
 import { Quizzes } from './Quizzes';
 
 export const Admin: React.FC = () => {
-  const [quizName, setQuizName] = useState<string>('');
+  // const [quizName, setQuizName] = useState<string>('');
   const [quizId, setQuizId] = useState<string>('');
 
-  const [
-    generateQuiz,
-    { called: generateQuizCalled, error: generateQuizErrors },
-  ] = useGenerateRandomQuizMutation({
-    variables: {
-      input: {
-        quizName,
-      },
-    },
-    onCompleted: (data) => setQuizId(data.generateRandomQuiz.quizId),
-  });
+  // const [
+  //   generateQuiz,
+  //   { called: generateQuizCalled, error: generateQuizErrors },
+  // ] = useGenerateRandomQuizMutation({
+  //   variables: {
+  //     input: {
+  //       quizName,
+  //     },
+  //   },
+  //   onCompleted: (data) => setQuizId(data.generateRandomQuiz.quizId),
+  // });
 
-  const generateQuizError = generateQuizErrors?.graphQLErrors?.[0];
+  // const generateQuizError = generateQuizErrors?.graphQLErrors?.[0];
 
   const [nextState] = useNextQuizStateMutation({
     variables: {
@@ -43,26 +43,26 @@ export const Admin: React.FC = () => {
         <Quizzes />
         <form>
           <h2>Generate random quiz</h2>
-          <input
-            value={quizName}
-            placeholder="Quiz name"
-            onChange={(e) => setQuizName(e.currentTarget.value)}
-          />
+          {/*<input*/}
+          {/*  value={quizName}*/}
+          {/*  placeholder="Quiz name"*/}
+          {/*  onChange={(e) => setQuizName(e.currentTarget.value)}*/}
+          {/*/>*/}
           <input
             value={quizId}
             placeholder="Quiz ID"
             onChange={(e) => setQuizId(e.currentTarget.value)}
           />
-          <button
-            disabled={generateQuizCalled && !generateQuizError}
-            onClick={() => generateQuiz()}
-          >
-            Generate quiz
-          </button>
-          {generateQuizError ? <p>{generateQuizError.message}</p> : null}
+          {/*<button*/}
+          {/*  disabled={generateQuizCalled && !generateQuizError}*/}
+          {/*  onClick={() => generateQuiz()}*/}
+          {/*>*/}
+          {/*  Generate quiz*/}
+          {/*</button>*/}
+          {/*{generateQuizError ? <p>{generateQuizError.message}</p> : null}*/}
         </form>
         <h2>Current quiz</h2>
-        <h3>Name: {quizName}</h3>
+        {/*<h3>Name: {quizName}</h3>*/}
         <h3>ID: {quizId}</h3>
         <button onClick={() => nextState()}>Next state</button>
       </section>
