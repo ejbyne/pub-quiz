9fe1b63037bba0e3263b53dd63cad1f7a624d0cd
Edward Byne <edward.byne@codecentric.de>
2021-04-24 23:22:20 +0200

    Add new quiz reducer logic

---
+++ shared/src/domain/newQuizReducer.ts
@@ -0,0 +1,133 @@
+import { NewQuizAction } from './newQuizTypes';
+import { SaveQuizInput } from '../graphql/types';
+
+const emptyQuiz = {
+  quizName: '',
+  rounds: [
+    {
+      roundName: '',
+      questions: [
+        {
+          text: '',
+          answer: '',
+        },
+      ],
+    },
+  ],
+};
+
+export const newQuizReducer = (
+  quiz: SaveQuizInput = emptyQuiz,
+  action: NewQuizAction,
+): SaveQuizInput => {
+  switch (action.type) {
+    case 'QuizNameAmended': {
+      return {
+        ...quiz,
+        quizName: action.payload.quizName,
+      };
+    }
+
+    case 'RoundAdded':
+      return {
+        ...quiz,
+        rounds: [
+          ...quiz.rounds,
+          {
+            roundName: '',
+            questions: [
+              {
+                text: '',
+                answer: '',
+              },
+            ],
+          },
+        ],
+      };
+
+    case 'RoundRemoved':
+      return {
+        ...quiz,
+        rounds: quiz.rounds.filter(
+          (_, index) => action.payload.roundNumber !== index,
+        ),
+      };
+
+    case 'RoundNameAmended': {
+      const rounds = [...quiz.rounds];
+      const { roundNumber, roundName } = action.payload;
+      rounds[roundNumber] = {
+        ...rounds[roundNumber],
+        roundName,
+      };
+      return {
+        ...quiz,
+        rounds,
+      };
+    }
+
+    case 'QuestionAdded': {
+      const { roundNumber } = action.payload;
+      return {
+        ...quiz,
+        rounds: quiz.rounds.map((round, index) => {
+          if (roundNumber !== index) {
+            return round;
+          }
+          return {
+            ...round,
+            questions: [
+              ...round.questions,
+              {
+                text: '',
+                answer: '',
+              },
+            ],
+          };
+        }),
+      };
+    }
+
+    case 'QuestionRemoved': {
+      const { roundNumber, questionNumber } = action.payload;
+      return {
+        ...quiz,
+        rounds: quiz.rounds.map((round, index) => {
+          if (roundNumber !== index) {
+            return round;
+          }
+          return {
+            ...round,
+            questions: round.questions.filter(
+              (_, index) => questionNumber !== index,
+            ),
+          };
+        }),
+      };
+    }
+
+    case 'QuestionAmended': {
+      const { roundNumber, questionNumber, text, answer } = action.payload;
+      return {
+        ...quiz,
+        rounds: quiz.rounds.map((round, index) => {
+          if (roundNumber !== index) {
+            return round;
+          }
+          const updatedQuestions = [...round.questions];
+          updatedQuestions[questionNumber] = {
+            text,
+            answer,
+          };
+          return {
+            ...round,
+            questions: updatedQuestions,
+          };
+        }),
+      };
+    }
+
+    default:
+      return quiz;
+  }
+};
--- web-app/src/components/NewQuiz.tsx
+++ web-app/src/components/NewQuiz.tsx
@@ -6,13 +6,43 @@ export const NewQuiz = () => {
     <Layout>
       <section className="w-full px-6 py-6 flex flex-col bg-indigo-900 lg:shadow-2xl lg:rounded-lg flex-grow text-gray-200 overflow-y-auto">
         <h1>New quiz</h1>
-        <form>
-          <input
-            className="text-input mb-4"
-            // value={quizId}
-            placeholder="Quiz name"
-            // onChange={(e) => setQuizId(e.currentTarget.value)}
-          />
+        <form className="flex flex-col">
+          <label>
+            Quiz name
+            <input
+              className="text-input mb-4"
+              // value={quizId}
+              // onChange={(e) => setQuizId(e.currentTarget.value)}
+            />
+          </label>
+          <label>
+            Round 1
+            <input
+              className="text-input mb-4"
+              // value={quizId}
+              // onChange={(e) => setQuizId(e.currentTarget.value)}
+            />
+          </label>
+          <div>
+            <p>Question 1</p>
+            <label>
+              Question
+              <input
+                className="text-input mb-4"
+                // value={quizId}
+                // onChange={(e) => setQuizId(e.currentTarget.value)}
+              />
+            </label>
+            <label>
+              Answer
+              <input
+                className="text-input mb-4"
+                // value={quizId}
+                // onChange={(e) => setQuizId(e.currentTarget.value)}
+              />
+            </label>
+          </div>
+          <button>Add question</button>
         </form>
       </section>
     </Layout>
