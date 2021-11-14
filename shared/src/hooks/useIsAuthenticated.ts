34e30cc70632a13c3b41a817bca672ec0bfdd5e5
Edward Byne <edward.byne@codecentric.de>
2021-11-14 16:33:56 +0100

    Fix path in useIsAuthenticated

--- shared/src/hooks/useIsAuthenticated.ts
+++ shared/src/hooks/useIsAuthenticated.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '@pub-quiz/web-app/src/components/IsAuthenticatedProvider';
+import { AuthContext } from '@pub-quiz/web-app/src/components/admin/IsAuthenticatedProvider';
 
 export const useIsAuthenticated = () => {
   return useContext(AuthContext);
